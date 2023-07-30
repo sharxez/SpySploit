09cbba6d76cd213f48fa5ec611806aa4098472a0
sharxez <77684405+sharxez@users.noreply.github.com>
2023-07-30 21:51:11 +0200

    Add files via upload

---
+++ Spy Sploit/Spy Sploit/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Spy_Sploit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(787, 78);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::Spy_Sploit.Properties.Resources.sol;
+            this.pictureBox2.Location = new System.Drawing.Point(704, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(69, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Spy_Sploit.Properties.Resources.Spy_PNG_Pic__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(59, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button1.Location = new System.Drawing.Point(5, 422);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Inject";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button2.Location = new System.Drawing.Point(5, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button3.Location = new System.Drawing.Point(5, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Execute";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button4.Location = new System.Drawing.Point(5, 177);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Open File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button5.Location = new System.Drawing.Point(5, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(149, 41);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Save Text";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button8.Location = new System.Drawing.Point(5, 328);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(149, 41);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Exit";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.DarkViolet;
+            this.button6.Location = new System.Drawing.Point(5, 375);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(149, 41);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Minimize";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel2.Controls.Add(this.fastColoredTextBox1);
+            this.panel2.Location = new System.Drawing.Point(160, 83);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(612, 380);
+            this.panel2.TabIndex = 9;
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fastColoredTextBox1.AutoIndentCharsPatterns = "^\\s*[\\w\\.]+(\\s\\w+)?\\s*(?<range>=)\\s*(?<range>[^;=]+);\r\n^\\s*(case|default)\\s*[^:]*" +
+    "(?<range>:)\\s*(?<range>[^;]+);";
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(335, 22);
+            this.fastColoredTextBox1.BackBrush = null;
+            this.fastColoredTextBox1.BookmarkColor = System.Drawing.Color.DarkViolet;
+            this.fastColoredTextBox1.CharHeight = 22;
+            this.fastColoredTextBox1.CharWidth = 12;
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold);
+            this.fastColoredTextBox1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.fastColoredTextBox1.IsReplaceMode = false;
+            this.fastColoredTextBox1.LineNumberColor = System.Drawing.Color.DarkViolet;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Paddings = new System.Windows.Forms.Padding(0);
+            this.fastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fastColoredTextBox1.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fastColoredTextBox1.ServiceColors")));
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(606, 373);
+            this.fastColoredTextBox1.TabIndex = 2;
+            this.fastColoredTextBox1.Text = "-- Welcome to, Spy Sploit";
+            this.fastColoredTextBox1.Zoom = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Spy_Sploit.Properties.Resources.sexpoopbuttballs__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(785, 477);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Spy Sploit";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fastColoredTextBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel2;
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Spy Sploit/Spy Sploit/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spy_Sploit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                openFileDialog1.Title = "Open";
+                fastColoredTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        Point lastPoint;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(fastColoredTextBox1.Text);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fastColoredTextBox1.Text = String.Empty;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int shit_aids_fuck = 0;
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            shit_aids_fuck = shit_aids_fuck + 1;
+
+            if (shit_aids_fuck == 1)
+            {
+                panel1.BackColor = Color.FromArgb(20, 20, 20);
+                panel2.BackColor = Color.FromArgb(12, 12, 12);
+                button3.BackColor = Color.FromArgb(20, 20, 20);
+                button2.BackColor = Color.FromArgb(20, 20, 20);
+                button4.BackColor = Color.FromArgb(20, 20, 20);
+                button5.BackColor = Color.FromArgb(20, 20, 20);
+                button8.BackColor = Color.FromArgb(20, 20, 20);
+                button6.BackColor = Color.FromArgb(20, 20, 20);
+                button1.BackColor = Color.FromArgb(20, 20, 20);
+                this.BackColor = Color.FromArgb(20, 20, 20);
+                fastColoredTextBox1.BackColor = Color.FromArgb(20, 20, 20);
+                fastColoredTextBox1.IndentBackColor = Color.FromArgb(20, 20, 20);
+
+                button3.FlatAppearance.BorderColor = Color.FromArgb(12, 12, 12);
+                button2.FlatAppearance.BorderColor = Color.FromArgb(12, 12, 12);
+                button4.FlatAppearance.BorderColor = Color.FromArgb(12, 12, 12);
+                button5.FlatAppearance.BorderColor = Color.FromArgb(12, 12, 12);
+                button8.FlatAppearance.BorderColor = Color.FromArgb(12, 12, 12);
+                button6.FlatAppearance.BorderColor = Color.FromArgb(12, 12, 12);
+                button1.FlatAppearance.BorderColor = Color.FromArgb(12, 12, 12);
+                fastColoredTextBox1.ServiceLinesColor = Color.FromArgb(12, 12, 12);
+                pictureBox2.Image = Spy_Sploit.Properties.Resources.piajauerd;
+                pictureBox2.Refresh();
+            }
+            if (shit_aids_fuck == 2)
+            {
+                panel1.BackColor = Color.White;
+                panel2.BackColor = Color.White;
+                button3.BackColor = Color.White;
+                button2.BackColor = Color.White;
+                button4.BackColor = Color.White;
+                button5.BackColor = Color.White;
+                button8.BackColor = Color.White;
+                button6.BackColor = Color.White;
+                button1.BackColor = Color.White;
+                this.BackColor = Color.White;
+                fastColoredTextBox1.BackColor = Color.White;
+                fastColoredTextBox1.IndentBackColor = Color.WhiteSmoke;
+
+                button3.FlatAppearance.BorderColor = Color.Silver;
+                button2.FlatAppearance.BorderColor = Color.Silver;
+                button4.FlatAppearance.BorderColor = Color.Silver;
+                button5.FlatAppearance.BorderColor = Color.Silver;
+                button8.FlatAppearance.BorderColor = Color.Silver;
+                button6.FlatAppearance.BorderColor = Color.Silver;
+                button1.FlatAppearance.BorderColor = Color.Silver;
+                fastColoredTextBox1.ServiceLinesColor = Color.Silver;
+                pictureBox2.Image = Spy_Sploit.Properties.Resources.sol;
+                pictureBox2.Refresh();
+                shit_aids_fuck = 0;
+            }
+        }
+    }
+}
+// GUI by str4nd, code by xanderdk/Ryan Gosling. discord.gg/dV2MmY5vw6
